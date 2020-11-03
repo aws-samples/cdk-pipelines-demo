c9bcae84c0a4398e01df26f5fe2d6c4be5ab0328
Florian CHAZAL <chazalf@amazon.com>
2020-11-03 17:57:30 +0100

    fix hooks

--- pipelines_webinar/canary/loop.ts
+++ pipelines_webinar/canary/loop.ts
@@ -1,14 +1,14 @@
 const AWS = require('aws-sdk');
 
 exports.startCanary = async () => {
-    const synthteticsClient = new AWS.Synthtecics();
+    const synthteticsClient = new AWS.Synthetics();
     return await synthteticsClient.startCanary({
         Name: process.env.CANARY_NAME
     }).promise();
 };
 
 exports.stopCanary = async () => {
-    const synthteticsClient = new AWS.Synthtecics();
+    const synthteticsClient = new AWS.Synthetics();
     return await synthteticsClient.stopCanary({
         Name: process.env.CANARY_NAME
     }).promise();
