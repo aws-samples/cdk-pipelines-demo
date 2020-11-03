8842a1a6cba5ffbd3479c18271e7b3c4bf7e7975
Florian CHAZAL <chazalf@amazon.com>
2020-11-03 18:12:57 +0100

    Add hook logs

--- pipelines_webinar/canary/loop.ts
+++ pipelines_webinar/canary/loop.ts
@@ -2,14 +2,19 @@ const AWS = require('aws-sdk');
 
 exports.startCanary = async () => {
     const synthteticsClient = new AWS.Synthetics();
-    return await synthteticsClient.startCanary({
+    console.log(`starting canary ${process.env.CANARY_NAME}`);
+    const result =  await synthteticsClient.startCanary({
         Name: process.env.CANARY_NAME
     }).promise();
+    console.log(`canary ${process.env.CANARY_NAME} started: ${JSON.stringify(result)}`);
+
 };
 
 exports.stopCanary = async () => {
     const synthteticsClient = new AWS.Synthetics();
-    return await synthteticsClient.stopCanary({
+    console.log(`stoping canary ${process.env.CANARY_NAME}`);
+    const result =  await synthteticsClient.stopCanary({
         Name: process.env.CANARY_NAME
     }).promise();
+    console.log(`canary ${process.env.CANARY_NAME} stopped: ${JSON.stringify(result)}`);
 };
