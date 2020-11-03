daefece52dfced31ea718756b714cd9b3bb9a9f2
Florian CHAZAL <chazalf@amazon.com>
2020-11-03 21:30:01 +0100

    add proper callback for deploy hook

--- pipelines_webinar/canary/loop.ts
+++ pipelines_webinar/canary/loop.ts
@@ -1,20 +1,47 @@
 const AWS = require('aws-sdk');
+const synthteticsClient = new AWS.Synthetics();
+const codedeploy = new AWS.CodeDeploy({apiVersion: '2014-10-06'});
 
-exports.startCanary = async () => {
-    const synthteticsClient = new AWS.Synthetics();
-    console.log(`starting canary ${process.env.CANARY_NAME}`);
-    const result =  await synthteticsClient.startCanary({
-        Name: process.env.CANARY_NAME
-    }).promise();
-    console.log(`canary ${process.env.CANARY_NAME} started: ${JSON.stringify(result)}`);
+exports.startCanary = async (event: any) => {
+    let result = 'pending';
+    try {
+        console.log(`starting canary ${process.env.CANARY_NAME}`);
+        await synthteticsClient.startCanary({
+            Name: process.env.CANARY_NAME
+        }).promise();
+        console.log(`canary ${process.env.CANARY_NAME} started`);
+        result = 'Succeeded';
+    } catch (error) {
+        result = 'Failed';
+    }
 
+
+    const params = {
+        deploymentId: event.DeploymentId,
+        lifecycleEventHookExecutionId: event.LifecycleEventHookExecutionId,
+        status: result // status can be 'Succeeded' or 'Failed'
+    };
+    codedeploy.putLifecycleEventHookExecutionStatus(params).promise();
 };
 
-exports.stopCanary = async () => {
-    const synthteticsClient = new AWS.Synthetics();
-    console.log(`stoping canary ${process.env.CANARY_NAME}`);
-    const result =  await synthteticsClient.stopCanary({
-        Name: process.env.CANARY_NAME
-    }).promise();
-    console.log(`canary ${process.env.CANARY_NAME} stopped: ${JSON.stringify(result)}`);
+exports.stopCanary = async (event: any) => {
+    let result = 'pending';
+    try {
+        console.log(`stoping canary ${process.env.CANARY_NAME}`);
+        await synthteticsClient.stopCanary({
+            Name: process.env.CANARY_NAME
+        }).promise();
+        console.log(`canary ${process.env.CANARY_NAME} stopped`);
+        result = 'Succeeded';
+    } catch (error) {
+        result = 'Failed';
+    }
+
+
+    const params = {
+        deploymentId: event.DeploymentId,
+        lifecycleEventHookExecutionId: event.LifecycleEventHookExecutionId,
+        status: result // status can be 'Succeeded' or 'Failed'
+    };
+    codedeploy.putLifecycleEventHookExecutionStatus(params).promise();
 };
