38d6a8189810c94610b32f16ffb9b1696d0d6769
Florian CHAZAL <chazalf@amazon.com>
2020-11-03 11:41:58 +0100

    Better rollback with  synthetic

--- pipelines_webinar/pipelines_webinar_stack.ts
+++ pipelines_webinar/pipelines_webinar_stack.ts
@@ -4,6 +4,7 @@ import * as lambda from '@aws-cdk/aws-lambda'
 import * as apigw from '@aws-cdk/aws-apigateway'
 import * as codedeploy from '@aws-cdk/aws-codedeploy'
 import * as cloudwatch from '@aws-cdk/aws-cloudwatch';
+import * as synthetics from '@aws-cdk/aws-synthetics';
 
 export class PipelinesWebinarStack extends Stack {
   urlOutput: CfnOutput;
@@ -27,19 +28,37 @@ export class PipelinesWebinarStack extends Stack {
       handler: alias,
     });
 
-    const apiGateway5xx = new cloudwatch.Metric({
-      metricName: '5XXError',
-      namespace: 'AWS/ApiGateway',
-      dimensions: {
-        ApiName: 'Gateway'
-      },
-      statistic: 'Sum',
-      period: Duration.minutes(1)
+    // const apiGateway5xx = new cloudwatch.Metric({
+    //   metricName: '5XXError',
+    //   namespace: 'AWS/ApiGateway',
+    //   dimensions: {
+    //     ApiName: 'Gateway'
+    //   },
+    //   statistic: 'Sum',
+    //   period: Duration.minutes(1)
+    // });
+
+    // const failureAlarm = new cloudwatch.Alarm(this, 'RollbackAlarm', {
+    //   metric: apiGateway5xx,
+    //   threshold: 1,
+    //   evaluationPeriods: 1,
+    // });
+
+    const canary = new synthetics.Canary(this, 'RegressionTesting', {
+      schedule: synthetics.Schedule.rate(Duration.minutes(1)),
+      test: synthetics.Test.custom({
+        code: synthetics.Code.fromAsset(path.join(__dirname, 'canary')),
+        handler: 'apiCall.handler',
+      }),
+      runtime: synthetics.Runtime.SYNTHETICS_NODEJS_2_0,
     });
-    const failureAlarm = new cloudwatch.Alarm(this, 'RollbackAlarm', {
-      metric: apiGateway5xx,
-      threshold: 1,
-      evaluationPeriods: 1,
+    // canary.node.addDependency(api);
+
+    const failureAlarm = new cloudwatch.Alarm(this, 'CanaryAlarm', {
+      metric: canary.metricSuccessPercent(),
+      evaluationPeriods: 2,
+      threshold: 90,
+      comparisonOperator: cloudwatch.ComparisonOperator.LESS_THAN_THRESHOLD,
     });
 
     new codedeploy.LambdaDeploymentGroup(this, 'DeploymentGroup ', {
