5dd1656e8738297e789be328d20e4fe5be38d738
Florian CHAZAL <chazalf@amazon.com>
2020-11-03 15:09:18 +0100

    fake failed deployment

--- pipelines_webinar/lambda/handler.ts
+++ pipelines_webinar/lambda/handler.ts
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 
 export const handler = async (event: any = {}): Promise<any> => {
     return {
-        body: 'OK its all good now',
-        statusCode: 200,
+        body: 'Test failure',
+        statusCode: 500,
     };
 };
