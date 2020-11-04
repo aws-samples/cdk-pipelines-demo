798be3898d3a2ee4eb06af4e6d6071a77311a39a
Florian CHAZAL <chazalf@amazon.com>
2020-11-04 11:53:45 +0100

    All good now

--- pipelines_webinar/lambda/handler.ts
+++ pipelines_webinar/lambda/handler.ts
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 
 export const handler = async (event: any = {}): Promise<any> => {
     return {
-        body: 'Back to normal',
+        body: 'All Good',
         statusCode: 200,
     };
 };
