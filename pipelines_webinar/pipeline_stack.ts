d28733f8be067a8e66d552d1bfa80157fa42a7e0
Florian CHAZAL <chazalf@amazon.com>
2020-11-04 12:28:36 +0100

    Add logs

--- pipelines_webinar/pipeline_stack.ts
+++ pipelines_webinar/pipeline_stack.ts
@@ -30,7 +30,7 @@ export class PipelineStack extends Stack {
             cloudAssemblyArtifact,
             sourceAction,
             synthAction,
-            selfMutating: false
+            selfMutating: true
         });
 
         // Pre-prod
