95f1599ee7296689e2dda813167042ec7561fec5
Florian CHAZAL <chazalf@amazon.com>
2020-11-13 17:18:52 +0100

    buggy update

--- pipelines_webinar/lambda/handler.ts
+++ pipelines_webinar/lambda/handler.ts
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 
 export const handler = async (event: any = {}): Promise<any> => {
     return {
-        body: 'OK',
-        statusCode: 200,
+        body: 'KO',
+        statusCode: 500,
     };
 };
