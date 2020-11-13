29c62870f87daf8ec4c501c257d6fa4d070f7276
Florian CHAZAL <chazalf@amazon.com>
2020-11-13 09:35:28 +0100

    v10 ko

--- pipelines_webinar/lambda/handler.ts
+++ pipelines_webinar/lambda/handler.ts
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 
 export const handler = async (event: any = {}): Promise<any> => {
     return {
-        body: 'v9',
-        statusCode: 200,
+        body: 'v10',
+        statusCode: 500,
     };
 };
