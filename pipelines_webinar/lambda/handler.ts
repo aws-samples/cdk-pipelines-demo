12174c5cdea6c3bf96786b85257dd1a62e6adfd3
Florian CHAZAL <chazalf@amazon.com>
2021-01-05 14:26:44 +0100

    Broken

--- pipelines_webinar/lambda/handler.ts
+++ pipelines_webinar/lambda/handler.ts
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 
 export const handler = async (event: any = {}): Promise<any> => {
     return {
-        body: 'Fixed',
-        statusCode: 200,
+        body: 'Broken',
+        statusCode: 500,
     };
 };
