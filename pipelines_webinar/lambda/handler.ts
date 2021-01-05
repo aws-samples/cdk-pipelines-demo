3fcc2a6839bf712a8bb0bbc860b7b509320357e6
Florian Chazal <florianchazal@gmail.com>
2021-01-05 21:10:28 +0100

    Re-fix code

--- pipelines_webinar/lambda/handler.ts
+++ pipelines_webinar/lambda/handler.ts
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 
 export const handler = async (event: any = {}): Promise<any> => {
     return {
-        body: 'Break',
-        statusCode: 500,
+        body: 'Fixed',
+        statusCode: 200,
     };
 };
