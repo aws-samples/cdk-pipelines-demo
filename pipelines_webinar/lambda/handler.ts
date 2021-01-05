cda1702e260c16017c19c8f6ba9853b7a3493991
Florian Chazal <florianchazal@gmail.com>
2021-01-05 21:29:16 +0100

    v1

--- pipelines_webinar/lambda/handler.ts
+++ pipelines_webinar/lambda/handler.ts
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 
 export const handler = async (event: any = {}): Promise<any> => {
     return {
-        body: 'Fixed',
+        body: 'v1',
         statusCode: 200,
     };
 };
