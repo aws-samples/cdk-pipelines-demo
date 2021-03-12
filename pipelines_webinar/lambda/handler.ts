cf9204b0be8a248e0863e94583bb3face327ff10
Wojciech Dąbrowski <wojc.dabrowski@gmail.com>
2021-03-12 09:29:48 +0100

    Correct integration test, make canary deployment shorter and make default lambda correct

--- pipelines_webinar/lambda/handler.ts
+++ pipelines_webinar/lambda/handler.ts
@@ -1,8 +1,8 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
 export const handler = async (event: any = {}): Promise<any> => {
-    return {
-        body: 'Broken',
-        statusCode: 500,
-    };
+  return {
+    body: "Hello",
+    statusCode: 200,
+  };
 };
